47af9bf93f2ac0a4daf8d3c2cb12440053944c9f
Viktoria Hetman <pvp071984@gmail.com>
2020-08-13 15:34:46 +0300

    Updates

---
+++ precache-manifest.c6e991f9581d432bd860247c93a373d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43515be50aa9ba2d58ada7ef25518d47",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "5e29c2f1fa97ec5e0e4d",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.79987094.chunk.css"
+  },
+  {
+    "revision": "7c760c7019bfcf7736dd",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.a3dfa0ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.a3dfa0ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e29c2f1fa97ec5e0e4d",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.02e21c70.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
